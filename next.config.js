f177ea35dd035fe5c0ba4692cfa490fb83f85213
hyunjin <hyunjin6918@naver.com>
2022-10-20 13:35:13 +0900

    path test

--- next.config.js
+++ next.config.js
@@ -4,10 +4,8 @@ const nextConfig = {
   swcMinify: true,
   images: {
     unoptimized: true,
-    path: "",
-  },
-  basePath: "/blog",
-  assetPrefix: "/blog"
+    path: ""
+  }
 };
 
 module.exports = nextConfig
