667f72adc376764be8e90960f707922783fb4741
hyunjin <hyunjin6918@naver.com>
2022-10-25 20:18:01 +0900

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
-    unoptimized: true
-  },
-  basePath: "/blog",
-  assetPrefix: "/blog",
-  images:{
+    unoptimized: true,
     domains: [
       'www.notion.so',
       'images.unsplash.com'
     ]
-  }
+  },
+  basePath: "/blog",
+  assetPrefix: "/blog",
 };
 
 module.exports = nextConfig
